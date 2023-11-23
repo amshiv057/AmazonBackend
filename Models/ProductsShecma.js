7b8b4b29f5d3d59ee1d69c7727ed4227100581c0
Shivam Tiwari <shivamtiwari.57.2020@gmail.com>
2023-11-24 02:24:15 +0530

    Initial commit

---
+++ Models/ProductsShecma.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose");
+// const products = require("../constent/Productsdata");
+
+const productsSchema= new mongoose.Schema({
+    id:String,
+    url:String,
+    detailUrl:String,
+    title:Object,
+    price:Object,
+    description:String,
+    discount:String,
+    tagline:String
+})
+
+const products= new mongoose.model("products",productsSchema);
+
+module.exports=products;
